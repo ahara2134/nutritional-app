30b1d454e2f3f6942adb4d425abe5ea3f161a2a8
Ryan <datmanwich@gmail.com>
2018-04-30 13:46:04 -0700

    Added the bottom navbar

--- app/src/main/java/ca/infostages/infonut/MainActivity.java
+++ app/src/main/java/ca/infostages/infonut/MainActivity.java
@@ -1,13 +1,25 @@
 package ca.infostages.infonut;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button yourButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        yourButton = (Button) findViewById(R.id.button);
+
+        yourButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, Home.class));
+            }
+        });
     }
 }
