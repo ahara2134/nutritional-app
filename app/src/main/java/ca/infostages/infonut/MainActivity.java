d2fd6bb5ab75b18219e25606facc732936697d2a
Sean Miyaoka <s.miyaoka@hotmail.com>
2018-05-01 10:18:54 -0700

    fixed bug

--- app/src/main/java/ca/infostages/infonut/MainActivity.java
+++ app/src/main/java/ca/infostages/infonut/MainActivity.java
@@ -7,12 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn = findViewById(R.id.scan_button);
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn = findViewById(R.id.scan_button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
