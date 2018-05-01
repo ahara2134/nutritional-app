4a64f9eebc54fe4ed02e3baae99960c0437bf806
smiyaoka <s.miyaoka@hotmail.com>
2018-04-30 23:15:22 -0700

    Started on barcode scanner

--- app/src/main/java/ca/infostages/infonut/MainActivity.java
+++ app/src/main/java/ca/infostages/infonut/MainActivity.java
@@ -1,13 +1,23 @@
 package ca.infostages.infonut;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn = findViewById(R.id.scan_button);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BarcodeReader.class));
+            }
+        });
     }
 }
