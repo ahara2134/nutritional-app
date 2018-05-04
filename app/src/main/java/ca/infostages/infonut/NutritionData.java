eb085f8ba6cb0f71382384c41aff74ee95450c88
Sean Miyaoka <s.miyaoka@hotmail.com>
2018-05-04 09:23:19 -0700

    Added JSON URL

---
+++ app/src/main/java/ca/infostages/infonut/NutritionData.java
@@ -0,0 +1,59 @@
+package ca.infostages.infonut;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NutritionData extends AsyncTask<Void,Void,Void>{
+    String data = "";
+    String barcode;
+    public NutritionData(String barcode) {
+        this.barcode = barcode;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            /*JSONArray JA = new JSONArray(data);
+            for (int i = 0; i < JA.length(); i++) {
+                JSONObject;
+            }*/
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        BarcodeReader.statusMessage.setText(this.data);
+    }
+}
