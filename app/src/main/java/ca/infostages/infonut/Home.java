b7739d29adc30a0da99a0d49b168e90815a995cd
aharaguchi <akharaguchi@gmail.com>
2018-05-08 11:08:07 -0700

    Home.java successfully sends user to NewUser.java if plan is not assigned.

--- app/src/main/java/ca/infostages/infonut/Home.java
+++ app/src/main/java/ca/infostages/infonut/Home.java
@@ -6,14 +6,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.data.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
 
     // Tags for the fragment manager
     private static final String TAG_CHOOSE_PLAN_FRAGMENT = "TAG_CHOOSE_PLAN_FRAGMENT";
     private static final String TAG_MAKE_PLAN_FRAGMENT = "TAG_MAKE_PLAN_FRAGMENT";
+    private static final String TAG = "Home.java";
 
     private TextView mTextMessage;
     private FragmentManager fragmentManager;
@@ -50,6 +63,28 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         // Inserts each fragment into the activity
+
+
+        //Checks if user's demographics are entered in. If not, send to NewUserActivity.
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference planReference;
+        planReference = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid()).child("plan");
+        planReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean plan = (boolean)dataSnapshot.getValue();
+                if (!plan) {
+                    Intent intent = new Intent (Home.this, NewUser.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, ": " + databaseError.getMessage());
+            }
+        });
+
 
         // ChoosePlanFragment
         ChoosePlanFragment choosePlanFragment = ChoosePlanFragment.newInstance();
