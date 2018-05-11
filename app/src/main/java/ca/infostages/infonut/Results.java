251b6e0a8649d9a580cd1642485550a48f5a3bca
aharaguchi <akharaguchi@gmail.com>
2018-05-11 11:55:02 -0700

    Removed google sign-in. Added signout button

--- app/src/main/java/ca/infostages/infonut/Results.java
+++ app/src/main/java/ca/infostages/infonut/Results.java
@@ -1,5 +1,6 @@
 package ca.infostages.infonut;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,23 +14,35 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class Results extends Fragment {
+public class Results extends Fragment implements View.OnClickListener {
 
     public static Results newInstance() {
         return new Results();
     }
+    Button button_bye;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstance) {
         View view = inflater.inflate(R.layout.fragment_results, container, false);
-
+        button_bye = (Button)view.findViewById(R.id.button_bye);
+        button_bye.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent (getActivity(), MainActivity.class);
+        startActivity(intent);
+        ((Activity)getActivity()).overridePendingTransition(0,0);
     }
 }
