5e329061ec4743dd561b6fdf2225938f3e821684
Ryan <datmanwich@gmail.com>
2018-05-17 12:08:18 -0700

    Fixed the back button on camera

--- app/src/main/java/ca/infostages/infonut/HomeFragment.java
+++ app/src/main/java/ca/infostages/infonut/HomeFragment.java
@@ -75,7 +75,7 @@ public class HomeFragment extends Fragment {
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(getActivity(), BarcodeCaptureActivity.class);
+                Intent intent2 = new Intent(getActivity(), BarcodeReader.class);
                 startActivity(intent2);
             }
         });
