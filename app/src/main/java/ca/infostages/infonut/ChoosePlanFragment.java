b0adc75a8857dfcb0a20b9e0f331c0f701db4276
aharaguchi <akharaguchi@gmail.com>
2018-05-18 12:15:22 -0700

    choosefragment edited

--- app/src/main/java/ca/infostages/infonut/ChoosePlanFragment.java
+++ app/src/main/java/ca/infostages/infonut/ChoosePlanFragment.java
@@ -11,14 +11,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Generates a view that allow the user to choose diet plans.
  */
-public class ChoosePlanFragment extends Fragment {
+public class ChoosePlanFragment extends Fragment implements View.OnClickListener{
 
     private Toolbar toolbar;
-
+    private Button defaultPlanDel, plan1Del, plan2Del, plan3Del, plan4Del, plan5Del;
+    private TextView defaultText, plan1Text, plan2Text, plan3Text, plan4Text, plan5Text;
+    private String plan1Holder, plan2Holder, plan3Holder, plan4Holder, plan5Holder;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference planReference, mDatabase;
     /**
      * Creates a new instance of this class and returns it to the caller.
      * @return a ChoosePlanFragment
@@ -34,20 +46,56 @@ public class ChoosePlanFragment extends Fragment {
         //Enable Back button
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-/*        toolbar = (Toolbar) getActivity().findViewById(R.id.toolbarId);
+        defaultPlanDel = (Button)view.findViewById(R.id.del_default);
+        plan1Del = (Button)view.findViewById(R.id.del_plan1);
+        plan2Del = (Button)view.findViewById(R.id.del_plan2);
+        plan3Del = (Button)view.findViewById(R.id.del_plan3);
+        plan4Del = (Button)view.findViewById(R.id.del_plan4);
+        plan5Del = (Button)view.findViewById(R.id.del_plan5);
+        defaultPlanDel.setOnClickListener(this);
+        plan1Del.setOnClickListener(this);
+        plan2Del.setOnClickListener(this);
+        plan3Del.setOnClickListener(this);
+        plan4Del.setOnClickListener(this);
+        plan5Del.setOnClickListener(this);
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment fragment = new HomeFragment();
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, fragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-            }
-        });*/
+        defaultText = (TextView)view.findViewById(R.id.default_plan);
+        plan1Text = (TextView)view.findViewById(R.id.plan1);
+        plan2Text = (TextView)view.findViewById(R.id.plan2);
+        plan3Text = (TextView)view.findViewById(R.id.plan3);
+        plan4Text = (TextView)view.findViewById(R.id.plan4);
+        plan5Text = (TextView)view.findViewById(R.id.plan5);
 
+        plan1Holder = "";
+        plan2Holder = "";
+        plan3Holder = "";
+        plan4Holder = "";
+        plan5Holder = "";
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+
+        }
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.del_default:
+                break;
+            case R.id.del_plan1:
+                break;
+            case R.id.del_plan2:
+                break;
+            case R.id.del_plan3:
+                break;
+            case R.id.del_plan4:
+                break;
+            case R.id.del_plan5:
+                break;
+        }
     }
 }
